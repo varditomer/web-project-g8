eddf8ef04f9c816c533002c2f72e4b0f8e56c35d
TOMER VARDI <79985779+varditomer@users.noreply.github.com>
2021-05-11 15:22:51 +0300

    Add files via upload

---
+++ js/items.js
@@ -0,0 +1,32 @@
+let rollsList = [
+    {
+        name: "גרין רול",
+        description: "אבוקדו, מלפפון, שעועית ירוקה ועירית",
+        price: 46,
+        img: "../Assets/grin.jpg",
+        type: "vegetarian"
+    },
+    {
+        name: "אספרה רול",
+        description: "אספרגוס, אבוקדו, מלפפון וגבינת שמנת בציפוי שומשום שחור",
+        price: 46,
+        img: "../Assets/aspara.jpg",
+        type: "vegetarian"
+
+    },
+    {
+        name: "רול צמחוני",
+        description: "מלפפון, קנפיו, גזר, בצל ירוק, טמאגו ואבוקדו",
+        price: 46,
+        img: "../Assets/veggie.jpg",
+        type: "vegetarian"
+
+    },
+    {
+        name: "רול בטטה קריספית",
+        description: "בטטה בטמפורה ואבוקדו בציפוי שבבי טמפורה",
+        price: 46,
+        img: "../Assets/batata.jpg",
+        type: "vegetarian"
+    }
+]
---
+++ js/payment.js
@@ -0,0 +1,122 @@
+function addTotalPrice() {
+    var totalPrice = localStorage.getItem("totalPrice");
+
+    $("#totalPrice").html(`סה"כ לתשלום: ₪${totalPrice}`);
+}
+
+function submitForm(event) {
+    event.preventDefault();
+
+    if (validateForm()) {
+        $('#success-order-modal').modal('show');
+    } else {
+        $('#error-toast').toast('show');
+    }
+}
+
+function validateForm() {
+    var isValid = true;
+
+    isValid = (checkName(document.getElementById("first-name")))? isValid : false;
+    isValid = (checkName(document.getElementById("last-name")))? isValid : false;
+    isValid = (checkPhoneNumber(document.getElementById("phone")))? isValid : false;
+    isValid = (checkName(document.getElementById("city")))? isValid : false;
+    isValid = (checkName(document.getElementById("street")))? isValid : false;
+    isValid = (checkNumber(document.getElementById("home-number")))? isValid : false;
+    isValid = (checkCreditCard())? isValid : false;
+
+    return isValid;
+}
+
+function checkCreditCard() {
+    var isValid = true;
+
+    if ($("#cardRadio").prop('checked')) {
+        isValid = checkCreditNum(document.getElementById('card-number'))? isValid : false;
+        isValid = checkCVV(document.getElementById("cvv"))? isValid : false;
+        isValid = checkId(document.getElementById("id-number"))? isValid : false;
+    }
+
+    return isValid;
+}
+
+function checkName(nameInput) {
+    var name = nameInput.value;
+    var lettersRegex = /^[A-Za-z]+$/;
+    var isValid = (name.length >= 2 && name.match(lettersRegex));
+    addValidationClass(nameInput, isValid);
+
+    return isValid;
+}
+
+function checkPhoneNumber(phoneInput) {
+    var phoneRegex = /^\d{10}$/;
+    var isValid = phoneInput.value.match(phoneRegex);
+    addValidationClass(phoneInput, isValid);
+
+    return isValid;
+}
+
+function checkNumber(numberInput) {
+    var numRegex = /^\d+$/;
+    var isValid = numberInput.value.match(numRegex);
+    addValidationClass(numberInput, isValid);
+
+    return isValid;
+}
+
+function checkCreditNum(creditNumInput) {
+    var creditRegex = /^\d{9,16}$/;
+    var isValid = creditNumInput.value.match(creditRegex);
+    addValidationClass(creditNumInput, isValid);
+    return isValid;
+}
+
+function checkCVV(cvvInput) {
+    var cvvRegex = /^\d{3}$/;
+    var isValid = cvvInput.value.match(cvvRegex);
+    addValidationClass(cvvInput, isValid);
+
+    return isValid;
+}
+
+function checkId(idInput) {
+    var idRegex = /^\d{9}$/;
+    var isValid = idInput.value.match(idRegex);
+    addValidationClass(idInput, isValid);
+
+    return isValid;
+}
+
+function addValidationClass(inputElement, isValid) {
+    if (isValid) {
+        inputElement.classList.remove('is-invalid');
+        inputElement.classList.add('is-valid');
+
+    } else {
+        inputElement.classList.remove('is-valid');
+        inputElement.classList.add('is-invalid');
+    }
+}
+
+function paymentKindChecked() {
+    var cardDetails = $("#cardDetails");
+
+    if ($("#cardRadio").prop('checked')) {
+        cardDetails.removeClass('visually-hidden')
+    } else {
+        cardDetails.addClass('visually-hidden')
+    }
+}
+
+function backToHomePage() {
+    window.location.href  = "../html/home.html"
+}
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    addTotalPrice();
+
+    $('#payment-form').on('submit', function(e) {
+        submitForm(e)
+    });
+})
